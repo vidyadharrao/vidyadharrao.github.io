b85f0d75779861fca9f3c513166984a497ced3ea
Vidyadhar Rao <6818557+vidyadharrao@users.noreply.github.com>
2025-03-29 18:21:46 +0530

    Update disable.js

--- js/disable.js
+++ js/disable.js
@@ -1,22 +1,26 @@
-document.addEventListener("copy", function(e) {
-    e.clipboardData.setData('text/plain', '');
-    e.preventDefault();
-}, false);
-
-document.addEventListener("cut", function(e) {
-    e.clipboardData.setData('text/plain', '');
-    e.preventDefault();
-}, false);
-
-document.addEventListener("contextmenu", function(e) {
-    e.preventDefault();
+document.addEventListener("mousedown", function(e) {
+    if (e.button === 0) {
+        e.preventDefault();
+    }
 }, false);
 
 document.addEventListener("keydown", function(e) {
+    if (e.ctrlKey && (e.key === 'a' || e.key === 'A')) {
+        e.preventDefault();
+    }
     if (e.ctrlKey && (e.key === 'c' || e.key === 'C')) {
         e.preventDefault();
     }
     if (e.ctrlKey && (e.key === 'x' || e.key === 'X')) {
+        e.preventDefault();
+    }
+    if (e.metaKey && (e.key === 'a' || e.key === 'A')) {
+        e.preventDefault();
+    }
+    if (e.metaKey && (e.key === 'c' || e.key === 'C')) {
+        e.preventDefault();
+    }
+    if (e.metaKey && (e.key === 'x' || e.key === 'X')) {
         e.preventDefault();
     }
     if (e.key === 'c' && e.metaKey) {
@@ -25,4 +29,21 @@ document.addEventListener("keydown", function(e) {
     if (e.key === 'x' && e.metaKey) {
         e.preventDefault();
     }
+    if (e.key === 'a' && e.metaKey) {
+        e.preventDefault();
+    }
+}, false);
+
+document.addEventListener("contextmenu", function(e) {
+    e.preventDefault();
+}, false);
+
+document.addEventListener("copy", function(e) {
+    e.clipboardData.setData('text/plain', '');
+    e.preventDefault();
+}, false);
+
+document.addEventListener("cut", function(e) {
+    e.clipboardData.setData('text/plain', '');
+    e.preventDefault();
 }, false);
