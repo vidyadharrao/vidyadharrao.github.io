1cf3f56d46b085f882db52bc5d33b3a4b8a315ce
Vidyadhar Rao <6818557+vidyadharrao@users.noreply.github.com>
2025-03-29 18:04:34 +0530

    Update disable.js

--- js/disable.js
+++ js/disable.js
@@ -1,3 +1,15 @@
+document.addEventListener("copy", function(e) {
+    e.preventDefault();
+    alert("Copying is not allowed on this webpage.");
+}, false);
+
 document.addEventListener("contextmenu", function(e) {
     e.preventDefault();
+}, false);
+
+document.addEventListener("keydown", function(e) {
+    if (e.ctrlKey && e.key === 'c') {
+        e.preventDefault();
+        alert("Copying is not allowed on this webpage.");
+    }
 }, false);
