22b5bfc45e41d6ede510879b4ded9654b8e7b077
Vidyadhar Rao <6818557+vidyadharrao@users.noreply.github.com>
2025-03-29 18:09:28 +0530

    Update disable.js

--- js/disable.js
+++ js/disable.js
@@ -1,6 +1,13 @@
 document.addEventListener("copy", function(e) {
     e.preventDefault();
+    e.clipboardData.clearData();
     alert("Copying is not allowed on this webpage.");
+}, false);
+
+document.addEventListener("cut", function(e) {
+    e.preventDefault();
+    e.clipboardData.clearData();
+    alert("Cutting is not allowed on this webpage.");
 }, false);
 
 document.addEventListener("contextmenu", function(e) {
@@ -8,8 +15,12 @@ document.addEventListener("contextmenu", function(e) {
 }, false);
 
 document.addEventListener("keydown", function(e) {
-    if (e.ctrlKey && e.key === 'c') {
+    if (e.ctrlKey && (e.key === 'c' || e.key === 'C')) {
         e.preventDefault();
         alert("Copying is not allowed on this webpage.");
+    }
+    if (e.ctrlKey && (e.key === 'x' || e.key === 'X')) {
+        e.preventDefault();
+        alert("Cutting is not allowed on this webpage.");
     }
 }, false);
