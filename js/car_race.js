af97f9edfbd2b0bbca0a935852108c8b563a292a
Vidyadhar Rao <6818557+vidyadharrao@users.noreply.github.com>
2025-06-04 14:38:44 +0530

    Update car_race.js

--- js/car_race.js
+++ js/car_race.js
@@ -9,6 +9,9 @@ var carSpeed = 5;
 var obstacleInterval = 1000;
 var obstacleWidth = 50;
 var obstacleHeight = 50;
+var score = 0;
+var level = 1;
+var gameInterval = null;
 
 // Game function
 function game() {
@@ -41,12 +44,42 @@ function game() {
         // Check for win condition
         if (carY > 550) {
                 alert('You win!');
+                clearInterval(gameInterval);
         }
 
         // Update game state
         carY += carSpeed;
-        setTimeout(game, 1000 / 60);
+        score++;
+        document.getElementById('score').innerHTML = 'Score: ' + score;
+
+        // Check for level completion
+        if (score % 100 === 0) {
+                level++;
+                obstacleInterval -= 50;
+                carSpeed += 0.5;
+        }
+
+        // Update game interval
+        if (!gameInterval) {
+                gameInterval = setInterval(game, obstacleInterval);
+        }
 }
+
+// Start game
+document.addEventListener('keydown', function(event) {
+        if (event.key === 'ArrowUp') {
+                carY -= 5;
+        }
+        if (event.key === 'ArrowDown') {
+                carY += 5;
+        }
+        if (event.key === 'ArrowLeft') {
+                carX -= 5;
+        }
+        if (event.key === 'ArrowRight') {
+                carX += 5;
+        }
+});
 
 // Start game
 game();
